bab76016bc7c1624e57a5906be32ff1b7ba9ab09
Grigoriy Efimov <efimov-gm@newpage.xyz>
2023-04-11 18:36:52 +0300

    CHange Loaing... to bootstrap spinner

--- src/resources/js/react/Loading/Loading.jsx
+++ src/resources/js/react/Loading/Loading.jsx
@@ -1,6 +1,11 @@
+import s from "./Loading.module.css"
 
 let Loading = () => {
-    return <div>Loading...</div>
+    return <div className="text-center">
+        <div className="spinner-border loading" role="status">
+        <span className="visually-hidden">Loading...</span>
+        </div>
+    </div>
 }
 
 export default Loading;
